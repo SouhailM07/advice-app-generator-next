caf2becd698e97e0d17b55c1c9f4b086943753ef
shadow <lightshadow416@gmail.com>
2023-10-29 19:25:38 +0100

    project core was build

---
+++ app/loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div id="loader-background">
+      <span id="loader">
+        <span id="loader-inner"></span>
+      </span>
+    </div>
+  );
+}
