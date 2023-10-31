fb3544ab6644b12683c0080da2fe18e6a1ea24ff
shadow <lightshadow416@gmail.com>
2023-10-31 08:04:00 +0100

    adding colors to tailwind and font manrope 800

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        LightCyan: "hsl(193, 38%, 86%)",
+        NeonGreen: "hsl(150, 100%, 66%)",
+        GrayishBlue: "hsl(217, 19%, 38%)",
+        DarkGrayishBlue: "hsl(217, 19%, 24%)",
+        DarkBlue: "hsl(218, 23%, 16%)",
       },
     },
   },
   plugins: [],
-}
+};
